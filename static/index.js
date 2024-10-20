1f9d01a1b1746543cce914c9804aa2ae0e8b6cc5
hateyoon <yoyo00830@gmail.com>
2024-10-21 00:33:07 +0900

    환경변수 확인

--- static/index.js
+++ static/index.js
@@ -1,3 +1,28 @@
+// let map;
+
+// function initMap() {
+//     const center = { lat: 37.6569377884, lng: 126.7718356549 }; // 기본 중심 좌표
+
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 15,
+//         center: center,
+//     });
+
+//     fetch('/toilets')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data); // 데이터 확인
+//             data.forEach(toilet => {
+//                 const marker = new google.maps.Marker({
+//                     position: { lat: toilet.latitude, lng: toilet.longitude },
+//                     map: map,
+//                     title: toilet.name,
+//                 });
+//             });
+//         })
+//         .catch(error => console.error('Error fetching toilet data:', error));
+// }
+
 let map;
 
 function initMap() {
@@ -18,8 +43,24 @@ function initMap() {
                     map: map,
                     title: toilet.name,
                 });
+
+                // InfoWindow 설정
+                const infoWindow = new google.maps.InfoWindow({
+                    content: `<h3>${toilet.name}</h3>`,
+                });
+
+                // 마커에 mouseover 이벤트 추가
+                marker.addListener('mouseover', () => {
+                    infoWindow.open(map, marker);
+                });
+
+                // 마커에 mouseout 이벤트 추가하여 InfoWindow 닫기
+                marker.addListener('mouseout', () => {
+                    infoWindow.close();
+                });
             });
         })
         .catch(error => console.error('Error fetching toilet data:', error));
 }
 
+initMap();
